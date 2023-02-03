91b6c938beb1bf2bb6ace2e54b014d547a93399b
BloodyMary8 <masharabota1996@gmail.com>
2023-02-03 18:18:53 +0700

    Get Token Shared Preference

--- app/src/main/java/com/chernikova/pethelp/di/AppComponent.kt
+++ app/src/main/java/com/chernikova/pethelp/di/AppComponent.kt
@@ -17,6 +17,7 @@ import com.chernikova.remote_module.RemoteProvider
     ]
 )
 interface AppComponent {
+
     //метод для того, чтобы появилась возможность внедрять зависимости в HomeFragmentViewModel
     fun inject(homeFragmentViewModel: HomeFragmentViewModel)
 
